9d144324f6b2006f78ad2dd61bb2468d4b5b275f
Alexis <alexis.cauchois@hetic.net>
2020-04-27 11:40:31 +0200

    elm tree created

--- front/src/Main.elm
+++ front/src/Main.elm
@@ -1,84 +1,18 @@
--- Press a button to generate a random number between 1 and 6.
---
--- Read how it works:
---   https://guide.elm-lang.org/effects/random.html
-
 module Main exposing (..)
 
-import Browser
-import Html exposing (..)
-import Html.Events exposing (..)
-import Random
+import Html
+import Types exposing (Model, Msg)
+import View exposing (view)
+import State exposing (init, update)
 
 
-
--- MAIN
-
-
+{-| Main entry point
+-}
+main : Program Never Model Msg
 main =
-  Browser.element
-    { init = init
-    , update = update
-    , subscriptions = subscriptions
-    , view = view
-    }
-
-
-
--- MODEL
-
-
-type alias Model =
-  { dieFace : Int
-  }
-
-
-init : () -> (Model, Cmd Msg)
-init _ =
-  ( Model 1
-  , Cmd.none
-  )
-
-
-
--- UPDATE
-
-
-type Msg
-  = Roll
-  | NewFace Int
-
-
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-  case msg of
-    Roll ->
-      ( model
-      , Random.generate NewFace (Random.int 1 6)
-      )
-
-    NewFace newFace ->
-      ( Model newFace
-      , Cmd.none
-      )
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  Sub.none
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-  div []
-    [ h1 [] [ text (String.fromInt model.dieFace) ]
-    , button [ onClick Roll ] [ text "Roll" ]
-    ]
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = always Sub.none
+        }
